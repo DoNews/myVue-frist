9f9bd8133abfd6bbc0d9676011f282a0de1c3ec6
861988926@qq.com <dss@peng#1314>
2019-12-10 15:59:22 +0800

    按时

--- src/main.js
+++ src/main.js
@@ -6,6 +6,8 @@ import 'element-ui/lib/theme-chalk/index.css'
 import App from './App'
 import router from './router'
 import echarts from 'echarts'
+// 懒加载
+import VueLazyload from 'vue-lazyload'
 import { sum1 } from './common/util'
 // import * as mufunction from './common/util' // 吧js内的所有方法导入到一个统一的字母里面
 // console.log(`公共函数：${mufunction.sum1}`) // 取值调用的话用这个对象调用
@@ -15,7 +17,16 @@ console.log(`公共函数：${sum1(5, 5)}`) // 10
 Vue.prototype.$echarts = echarts
 Vue.use(ElementUI)
 Vue.config.productionTip = false
-
+// 懒加载调用
+Vue.use(VueLazyload)
+// 带参数的懒加载样式
+// or with options
+// Vue.use(VueLazyload, {
+//   preLoad: 1.3,
+//   error: 'dist/error.png',
+//   loading: 'dist/loading.gif',
+//   attempt: 1
+// })
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
